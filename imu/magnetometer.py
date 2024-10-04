8442413d1a4afa95a5bfd0d24a17895601279d14
DevGuruPro <EricMoller4320@outlook.com>
2024-10-04 12:27:28 +1000

    magnetometer and sensor calibration working

--- imu/magnetometer.py
+++ imu/magnetometer.py
@@ -1,28 +1,53 @@
+glydeautomation@raspberrypi:~/AutoSlasher-/imu $ cat magnetometer.py 
 # sensors/magnetometer.py
-
-from utils.i2c_interface import I2CInterface
+import smbus
+import time
+from calibration import CalibrationManager
 
 class MagnetometerSensor:
     def __init__(self, address=0x0C, bus_num=1):
-        self.i2c = I2CInterface(bus_num)
         self.address = address
-        self.configure_sensor()
+        self.bus = smbus.SMBus(bus_num)
+        self.initialize_sensor()
+        self.calibrationManager = CalibrationManager()
+        self.calibrationManager.load_calibration('settings.py')
 
-    def configure_sensor(self):
-        # Initialize sensor settings
-        pass
+    def initialize_sensor(self):
+        # Soft reset the sensor
+        #self.write_byte(0x0B, 0x80)
+        time.sleep(0.1)
+        # Set measurement mode, change 0x01 to other values based on desired configuration
+        self.write_byte(0x0A, 0x01)
 
-    def read_raw_data(self):
-        # Read raw X, Y, Z data from the sensor
-        x = self._read_axis_data(0x03)
-        y = self._read_axis_data(0x05)
-        z = self._read_axis_data(0x07)
-        return x, y, z
+    def write_byte(self, reg, value):
+        self.bus.write_byte_data(self.address, reg, value)
 
-    def _read_axis_data(self, reg):
-        high = self.i2c.read_byte(self.address, reg)
-        low = self.i2c.read_byte(self.address, reg + 1)
-        value = (high << 8) | low
-        if value > 32767:
+    def read_byte(self, reg):
+        return self.bus.read_byte_data(self.address, reg)
+
+    def read_two_bytes(self, lsb_reg, msb_reg):
+        lsb = self.read_byte(lsb_reg)
+        msb = self.read_byte(msb_reg)
+        value = (msb << 8) + lsb
+        if value >= 32768:
             value -= 65536
         return value
+
+    def read_axis_data(self):
+        x = self.read_two_bytes(0x00, 0x01)
+        y = self.read_two_bytes(0x02, 0x03)
+        z = self.read_two_bytes(0x04, 0x05)
+        return x, y, z
+
+    def read_calibrated_data(self):
+        x, y, z = self.read_axis_data()
+        calibrated_x, calibrated_y, calibrated_z = self.calibrationManager.apply_calibration(x, y, z)
+        return calibrated_x, calibrated_y, calibrated_z
+
+    def read_heading(self):
+        calibrated_x, calibrated_y, _ = self.read_calibrated_data()
+        heading = self.calibrationManager.calculate_heading(calibrated_x, calibrated_y)
+        return heading
+
+    def __del__(self):
+        self.bus.close()
